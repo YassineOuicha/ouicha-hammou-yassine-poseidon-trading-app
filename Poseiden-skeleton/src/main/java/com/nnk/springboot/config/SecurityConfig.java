68e3c11004b5b6a1fd76220b480d71b030290102
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-20 00:18:28 +0100

    Correcting errors in BidList files (controler, templates ...)

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SecurityConfig.java
@@ -76,4 +76,7 @@ public class SecurityConfig {
 //
 //        return http.build();
 //    }
+
+
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -12,29 +12,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
-
 @Controller
+@RequestMapping("/bidList")
 public class BidListController {
 
     @Autowired
     private BidListService bidListService;
 
-    @RequestMapping("/bidList/list")
+    @RequestMapping("/list")
     public String home(Model model) {
         model.addAttribute("bidLists", bidListService.getAllBidList());
         return "bidList/list";
     }
 
-    @GetMapping("/bidList/add")
+    @GetMapping("/add")
     public String addBidForm(BidList bid) {
         return "bidList/add";
     }
 
-    @PostMapping("/bidList/validate")
+    @PostMapping("/validate")
     public String validate(@Valid BidList bid, BindingResult result, Model model) {
-        if ( result.hasErrors()){
+        if (result.hasErrors()) {
             return "bidList/add";
         }
         bidListService.saveBidList(bid);
@@ -42,18 +40,17 @@ public class BidListController {
         return "redirect:/bidList/list";
     }
 
-    @GetMapping("/bidList/update/{id}")
+    @GetMapping("/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        BidList bidList = bidListService.getBidListById(id)
-                .orElseThrow(()-> new RuntimeException("Invalid BidList Id : " + id ));
+        BidList bidList = bidListService.getBidListById(id).orElseThrow(() -> new IllegalArgumentException("Invalid bidList Id:" + id));
         model.addAttribute("bidList", bidList);
         return "bidList/update";
     }
 
-    @PostMapping("/bidList/update/{id}")
+    @PostMapping("/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList,
-                             BindingResult result, Model model) {
-        if (result.hasErrors()){
+                            BindingResult result, Model model) {
+        if (result.hasErrors()) {
             return "bidList/update";
         }
         bidList.setBidListId(id);
@@ -62,10 +59,9 @@ public class BidListController {
         return "redirect:/bidList/list";
     }
 
-    @GetMapping("/bidList/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        BidList bidList = bidListService.getBidListById(id)
-                .orElseThrow(()-> new RuntimeException("Invalid BidList Id : " + id ));
+        BidList bidList = bidListService.getBidListById(id).orElseThrow(() -> new IllegalArgumentException("Invalid bidList Id:" + id));
         bidListService.deleteBidListById(id);
         model.addAttribute("bidLists", bidListService.getAllBidList());
         return "redirect:/bidList/list";
