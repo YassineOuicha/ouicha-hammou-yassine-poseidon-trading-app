2b04cbc9ec715b2722055d5239cdc32162d06c63
Yassine Ouicha Hammou <yassineouichahammou@gmail.com>
2025-02-24 16:44:31 +0100

    Adding mandatory annotation to parameters so we have to enter a value in the front end view

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -31,54 +31,71 @@ public class BidList {
     @Column(name = "ask_quantity")
     private double askQuantity;
 
+    @NotBlank(message = "Bid is mandatory")
     @Column(name = "bid")
     private double bid;
 
+    @NotBlank(message = "Ask is mandatory")
     @Column(name = "ask")
     private double ask;
 
+    @NotBlank(message = "Benchmark is mandatory")
     @Column(name = "benchmark")
     private String benchmark;
 
+    @NotBlank(message = "Bid List Date is mandatory")
     @Column(name = "bid_list_date")
     private Timestamp bidListDate;
 
+    @NotBlank(message = "Commentary is mandatory")
     @Column(name = "commentary")
     private String commentary;
 
+    @NotBlank(message = "Security is mandatory")
     @Column(name = "security")
     private String security;
 
+    @NotBlank(message = "Status is mandatory")
     @Column(name = "status")
     private String status;
 
+    @NotBlank(message = "Trader is mandatory")
     @Column(name = "trader")
     private String trader;
 
+    @NotBlank(message = "Book is mandatory")
     @Column(name = "book")
     private String book;
 
+    @NotBlank(message = "Creation Name is mandatory")
     @Column(name = "creation_name")
     private String creationName;
 
+    @NotBlank(message = "Creation Date is mandatory")
     @Column(name = "creation_date")
     private Timestamp creationDate;
 
+    @NotBlank(message = "Revision Name is mandatory")
     @Column(name = "revision_name")
     private String revisionName;
 
+    @NotBlank(message = "Revision Date is mandatory")
     @Column(name = "revision_date")
     private Timestamp revisionDate;
 
+    @NotBlank(message = "Deal Name is mandatory")
     @Column(name = "deal_name")
     private String dealName;
 
+    @NotBlank(message = "Deal Type is mandatory")
     @Column(name = "deal_type")
     private String dealType;
 
+    @NotBlank(message = "Source List Id is mandatory")
     @Column(name = "source_list_id")
     private String sourceListId;
 
+    @NotBlank(message = "Side is mandatory")
     @Column(name = "side")
     private String side;
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/CurvePoint.java
@@ -1,6 +1,7 @@
 package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import java.sql.Timestamp;
 
@@ -14,18 +15,23 @@ public class CurvePoint {
     @Column(name = "id")
     private int id;
 
+    @NotBlank(message = "Curve Id is mandatory")
     @Column(name = "curve_id")
     private int curveId;
 
+    @NotBlank(message = "As Of Date is mandatory")
     @Column(name = "as_of_date")
     private Timestamp asOfDate;
 
+    @NotBlank(message = "Term is mandatory")
     @Column(name = "term")
     private Double term;
 
+    @NotBlank(message = "Value is mandatory")
     @Column(name = "value")
     private Double value;
 
+    @NotBlank(message = "Creation Date is mandatory")
     @Column(name = "creation_date")
     private Timestamp creationDate;
 }
